156c9794b0fd973527b3a0896995a662587f3ba1
Michael Han <hanhaotian87@126.com>
2021-04-14 16:27:16 +0800

    logger 去掉内存值，error打印stack

--- core/common/logger.js
+++ core/common/logger.js
@@ -1,4 +1,3 @@
-
 'use strict'
 const path = require('path')
 const log4js = require('koa-log4')
@@ -10,14 +9,13 @@ const LOG4JS_DEV_CONF = 'log4js-dev.json'
 const LOG4JS_PRD_CONF = 'log4js.json'
 
 // log4js 配置段
-function setupLog4js () {
+function setupLog4js() {
   console.log('setupLog4js : ' + process.env.NODE_ENV)
-  const fileName = (process.env.NODE_ENV === 'production')
-    ? LOG4JS_PRD_CONF
-    : LOG4JS_DEV_CONF
+  const fileName =
+    process.env.NODE_ENV === 'production' ? LOG4JS_PRD_CONF : LOG4JS_DEV_CONF
   log4js.configure(commonUtil.getConfigPath() + '/' + fileName, {
     cwd: appBase,
-    reloadSecs: CONFIG_RELOAD_SECS
+    reloadSecs: CONFIG_RELOAD_SECS,
   })
 }
 
@@ -28,7 +26,7 @@ exports.logger = function (category) {
   let c = category
   if (c && c.substr(0, appBase.length) === appBase) {
     c = path.relative(appBase, c)
-    c = '[' + process.pid + '] [rss ' + parseInt(process.memoryUsage().rss / 1024) + ']' + c
+    c = '[' + process.pid + ']' + c
   }
   return log4js.getLogger(c)
 } // 记录所有应用级别的日志
--- routes/error.js
+++ routes/error.js
@@ -4,11 +4,12 @@ module.exports = async (ctx, next) => {
   try {
     await next()
     // logger.error('status' + ctx.status + ' err ' + ctx.message)
-    if (ctx.status >= 400 && ctx.status < 600) { // 请求接口没有时被调用
+    if (ctx.status >= 400 && ctx.status < 600) {
+      // 请求接口没有时被调用
       ctx.throw(ctx.status, { retCode: 404, message: ctx.message })
     }
   } catch (err) {
-    logger.error('route error ' + err)
+    logger.error('route error ' + err.stack) //打印错误栈
     const status = err.status || 500
     ctx.status = status
     ctx.body = { retCode: err.retCode, message: err.message }
